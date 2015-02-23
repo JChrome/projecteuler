9f1ce72fe3660d0fc4e1593189a28744cacad0a1
JChrome <jmkrumsick@gmail.com>
2015-02-22 23:00:15 -0500

    fibbz

--- 2.c
+++ 2.c
@@ -4,8 +4,8 @@ int main(int argc, char **argv){
 	int start = 1;
 	int previous = 2;
 	int current = 0;
-	while(current < 4000000){
-
+	int specialk = 0;
+	while(current < 40){
 		current = start + previous;
 		start = previous;
 		previous = current;
@@ -14,6 +14,10 @@ int main(int argc, char **argv){
 			previous, 
 			start + previous, 
 			current);
+		if (current % 2 == 0){
+			specialk += current;
+			printf("total is == %d\n", specialk);
+		}
 	}
 	return 0;
 }
